1c753e26f3e6059f3b38bb22f1b226b1dd306deb
Jezyglee <157793047+Jezyglee@users.noreply.github.com>
2026-01-30 15:57:47 +0100

    Update mouse Position

--- Assets/GameObject/Formes/FormController.cs
+++ Assets/GameObject/Formes/FormController.cs
@@ -8,12 +8,25 @@ public class FormController : MonoBehaviour
     [SerializeField] private LayerMask _layerMask;
 
     private bool _hasClickedOnPlateform = false;
+    private bool _isOnPlateform = false;
 
     [SerializeField] private EColor _actualColor = EColor.VOID;
     public EColor ActualColor { get => _actualColor; set => _actualColor = value; }
 
     private void Update()
     {
+        GetInfo();
+    }
+
+    private void GetInfo()
+    {
+        InputManager input = InputManager.Instance; 
+
+        if (input.WasDragButtonPressed && _isOnPlateform)
+        {
+            _hasClickedOnPlateform = true;
+        }
+
         if (_hasClickedOnPlateform)
         {
             FollowMouse();
@@ -25,13 +38,22 @@ public class FormController : MonoBehaviour
         Vector3 mousePosition = InputManager.Instance.MousePosition;
         mousePosition.z = Mathf.Abs(Camera.main.transform.position.z);
 
+        float speed = 10f;
+
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = worldPosition;
+        worldPosition.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, worldPosition, 1f - Mathf.Exp(-speed * Time.deltaTime));
     }
 
     private void OnMouseDown()
     {
         _hasClickedOnPlateform = true;  
+    }
+
+    private void OnMouseOver()
+    {
+        _isOnPlateform = true; 
     }
 }
 
